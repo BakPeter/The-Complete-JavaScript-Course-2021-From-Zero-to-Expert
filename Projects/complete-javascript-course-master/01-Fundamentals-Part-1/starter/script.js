bd301fed567b360b9e2397db86648d1c53fae3f7
BakPeter <bak.pet@gmail.com>
2021-06-07 07:30:38 +0300

    initial commit

---
+++ Projects/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,16 @@
+let firstName = "Jonas knin";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.14;
+
+let isIsLand = false;
+let country
+console.log(isIsLand);
+console.log(country);
+let num = 23;
+console.log(num);
+num = "jbjbj";
+console.log(num);
