72202f91b8908dc3a7aec0b06b8d00734cea0666
BakPeter <bak.pet@gmail.com>
2021-06-15 17:14:12 +0300

    Section 2 : JavaScript Fundametals - Part 2

---
+++ Projects/complete-javascript-course-master/02-Fundamentals-Part-2/challenges/script.js
@@ -0,0 +1,97 @@
+'use strict'
+// alert("Foudemetaks part 2")
+
+// const interfcae = true;
+// console.log(interfcae)
+
+// logger()
+
+// function logger(){
+//     console.log("logger function")
+// }
+
+// logger()
+// logger()
+
+// const friend1 = "Michael"
+// const friend2 = "Steven"
+// const friend3 = "Peter"
+
+// const friends = [friend1, friend2, friend3, "eli"]
+// console.log(friends)
+
+// const years = new Array(1991,32, 56)
+// console.log(years)
+
+// console.log(friends[0])
+// console.log(years[2])
+// console.log(friends.length)
+// console.log(friends[0] = "TEst")
+// console.log(friends[0])
+// console.log(friends)
+
+// console.log(friends.push("test push"))
+// console.log(friends)
+// console.log(friends.unshift("unshift"))
+// console.log(friends)
+// console.log(friends.pop())
+// console.log(friends);
+// friends.includes
+
+// const jonas = {
+//     firstName: "jonas",
+//     lastName: "efe",
+//     birthYear: 1991,
+//     job: "teacher",
+//     friends: ["freind1","freind2", "friend3"],
+//     innerObject:{
+//         name: "innerName",
+//         age: 45
+//     },
+
+//     calcAge: function() {
+//         this.age = 2037 - this.birthYear 
+//         return this.age
+//      }
+// }
+
+// console.log(jonas)
+// console.log(jonas.firstName)
+// console.log(jonas['age'])
+// console.log(jonas['agefgf'])
+// console.log(jonas['last' + 'Name'])
+// console.log(jonas['friends'])
+// console.log(jonas['friends'][0])
+// console.log(jonas['innerObject']['age'])
+// console.log(jonas['innerObject'].name)
+// console.log(jonas.calcAge())
+// console.log(jonas.age)
+
+// const jonas = [
+//     "jonas",
+//     "efe",
+//     1991,
+//     "teacher",
+//     true,
+//     ["freind1","freind2", "friend3"],
+//     {
+//         name: "innerName",
+//         age: 45
+//     }
+// ]
+// console.log(jonas)
+
+// const types = []
+// for (let i = 0; i < jonas.length; i++) {
+//     const parameterType = typeof jonas[i];
+
+//     if(parameterType === 'string') continue
+    
+//     console.log(types.push(parameterType))
+//     if(parameterType === 'object') break
+
+// }
+// console.log(types)
+
+let dice = Math.trunc(Math.random() * 7)
+console.log(dice)
